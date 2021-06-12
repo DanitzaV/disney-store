0e62046c0286b3e74250739d9d9143da3882243d
viviana <vmarcelo@ecomsur.com>
2021-06-11 23:32:49 -0400

    reorganizar

---
+++ src/components/Layout/Footer/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <footer>Footer</footer>
+    )
+}
+
+export { Header }
---
+++ src/components/Layout/Header/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <nav>
+           <ul>
+               <li>
+                   Home
+               </li>
+               <li>
+                   Marvel
+               </li>
+           </ul>
+        </nav>
+    )
+}
+
+export { Header }
